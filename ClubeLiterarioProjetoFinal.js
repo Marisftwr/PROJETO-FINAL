b766dea530a0a3b0498280435ba0922a748c78db
Mari <146585679+Marisftwr@users.noreply.github.com>
2024-12-15 18:27:37 -0300

    Add files via upload
    Mais uploads

---
+++ ClubeLiterarioProjetoFinal.js
@@ -0,0 +1,9 @@
+
+function Entrar() {
+    var x = document.getElementById("x7");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
