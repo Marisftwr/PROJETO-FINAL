874dfd5acf4f7dd9bb4a3ef3b05cdfe4d2e4e62c
Mari <146585679+Marisftwr@users.noreply.github.com>
2025-01-05 21:07:26 -0300

    Update and rename PaginaDePerfil.html to PaginaDePerfil.php

--- PaginaDePerfil.php
+++ PaginaDePerfil.php
@@ -1,3 +1,6 @@
+<?php
+include('protect.php')
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <meta charset="UTF-8">
@@ -18,10 +21,10 @@
     <title>Perfil</title>
 </head>
 <body class="bodyPP">
-    <nav class="container-fluid">
+    <nav class="container">
         <ul class="row">
-          <a class="col-sm-3">
-            <img id="x1" src="Imagens\LogoSite.png">
+          <a class="col-mb-4">
+            <img class="img-fluid" src="Imagens\LogoSite.png">
           </a>
           <li class="col-sm-2">
             <a class="nav-link" href="Home.php">Home</a>
@@ -51,7 +54,10 @@
             <br><br>
             <div id="mySidenav2" class="sidenav">
                 <a class="quicksand-regular2" href="javascript:void(0)" class="closebtn" onclick="closeNav2()"><img id="fechar" src="Imagens\fechar.png"></a><br><br>
-                <a href="PaginaDePerfil.html"><img src="https://via.placeholder.com/50" class="rounded-circle me-3" alt="User Avatar"></a><br><br>
+                <a href="PaginaDePerfil.php"><img src="https://via.placeholder.com/50" class="rounded-circle me-3" alt="User Avatar"></a><br>
+                <p class="quicksand-regular2"><?php echo $_SESSION['nome']; ?></p><br>
+                <input class="form-control col-sm-10" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success col-sm-1" type="submit"><img id="BotaoPesquisa" src="Imagens\SearchIcon1.png"></button>
                 <a class="quicksand-regular2" href="QuemSomos.html">Quem Somos?</a><br><br>
                 <a class="quicksand-regular2" href="#">Minhas Listas</a><br><br>
                 <a class="quicksand-regular2" href="#">Avaliações</a><br><br>
@@ -59,14 +65,14 @@
                 <a class="quicksand-regular2" href="#">Configurações</a><img id="config" src="Imagens\configurações.png"><br><br>
                 <a class="quicksand-regular2" href="#">Sair</a><br><br>
             </div>
-        <div class="container-fluid">
+        <div class="container">
             <div class="HeaderUser">
                 <img id="ProfilePic"src="Imagens\profile picture.jpg">
                 <div class="linhaV"></div>
             </div><br>
             <ul>
-              <h3 id="x17" class="quicksand-regular">Nome</h3>
-            <p id="x17" class="quicksand-regular2">@nomedeusuario</p>
+              <h3 id="x17" class="quicksand-regular"><?php echo $_SESSION['nome']; ?></h3>
+            <p id="x17" class="quicksand-regular2"><?php echo $_SESSION['nome']; ?></p>
           </ul><br><br>
             <div class="livrosLidos">
             <h4 id="x21" class="quicksand-regular">Livros lidos:</h4>
@@ -80,7 +86,7 @@
             <div class="card" style="width: 20rem;">
             <div class="card-body">
             <h5 class="card-title">Livros:</h5><br>
-            <a class="col-sm-2" href="PáginasDeLeituraLivro.html"><img id="x6" src="Imagens\NovaImagemTesteLivroParaFicarBonito.jpg" alt="Livro1"></a><br><br>
+            <a class="col-sm-2" href="PáginasDeLeituraLivro.html"><img class="img-fluid" id="x6" src="Imagens\NovaImagemTesteLivroParaFicarBonito.jpg" alt="Livro1"></a><br><br>
             <a href="#" class="card-link">Nota</a>
             </div>
             </div>
@@ -91,7 +97,7 @@
             <div class="card" style="width: 20rem;">
               <div class="card-body">
                 <h5 class="card-title">Livros:</h5><br>
-                <a class="col-sm-2" href="PáginasDeLeituraLivro.html"><img id="x6" src="Imagens\NovaImagemTesteLivroParaFicarBonito.jpg" alt="Livro1"></a><br><br>
+                <a class="col-sm-2" href="PáginasDeLeituraLivro.html"><img class="img-fluid" id="x6" src="Imagens\NovaImagemTesteLivroParaFicarBonito.jpg" alt="Livro1"></a><br><br>
                 <a href="#" class="card-link">Nome do livro</a>
               </div>
           </div>  
@@ -101,7 +107,7 @@
           <div class="card" style="width: 20rem;">
           <div class="card-body">
             <h5 class="card-title">Livros:</h5><br>
-            <a class="col-sm-2" href="PáginasDeLeituraLivro.html"><img id="x6" src="Imagens\NovaImagemTesteLivroParaFicarBonito.jpg" alt="Livro1"></a><br><br>
+            <a class="col-sm-2" href="PáginasDeLeituraLivro.html"><img class="img-fluid" id="x6" src="Imagens\NovaImagemTesteLivroParaFicarBonito.jpg" alt="Livro1"></a><br><br>
             <a href="PáginasDeLeituraLivro.html" class="card-link">Nome Livro</a>
           </div>
           </ul>
